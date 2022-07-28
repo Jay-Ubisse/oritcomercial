dd491fa853219c17938c95de071ede5c3c371ccf
Jay Ubisse <ubisse.joaquim@outlook.com>
2022-07-28 01:16:55 -0700

    adding responsive design

--- assets/scripts/js/index.js
+++ assets/scripts/js/index.js
@@ -1,10 +1,24 @@
 $(document).ready(function() {
-    $('.sm').smartmenus({
-      showFunction: function($ul, complete) {
-        $ul.slideDown(250, complete);
-      },
-      hideFunction: function($ul, complete) {
-       $ul.slideUp(250, complete); 
+  $('.sm').smartmenus({
+    showFunction: function($ul, complete) {
+      $ul.slideDown(250, complete);
+    },
+    hideFunction: function($ul, complete) {
+      $ul.slideUp(250, complete); 
+    }
+  });
+
+  $("#menu-icon").hide();
+
+  if($(window).width() < 900){
+    $("#menu-icon").show();
+    $("#menu").hide();
+    $("#menu-icon").click(function () {
+      if ($("#menu").is(':hidden')) {
+        $("#menu").slideDown(300);
+      } else {
+        $("#menu").slideUp(300);
       }
     });
-  }); // end ready
+  }
+}); // end ready
